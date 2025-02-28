c773c5c4c41eff4445fa43920004ddb6c3e02ac7
nkepling <nathankep@gmail.com>
2025-02-28 03:40:38 -0600

    vin mcts results

--- experiments/vin_mcts.py
+++ experiments/vin_mcts.py
@@ -15,7 +15,7 @@ def new_mcts_agent(env,state,config,seed=None):
         vin = VIN(SimpleNamespace(**config))
         vin_weights = torch.load(config["vin_model_weights"],weights_only=True, map_location=config["device"])
 
-        vin = vin.load_state_dict(vin_weights)
+        vin.load_state_dict(vin_weights)
 
         return MCTS(env=env,
                     state=state,
